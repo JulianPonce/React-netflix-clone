bc8a00b8e272c08ffe44150fbf23fbe526cbcea8
Julian Ponce <juliangponce@gmail.com>
2022-02-21 13:53:16 -0300

    primer commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,236 @@
+import "./Home.scss";
+import NavBar from "../Componentes/NavBar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <NavBar />
+      <img
+      width="100%"
+        src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2014/10/391782-cine-superheroes-critica-batman-begins.jpg?itok=MkAZ9Rll"
+        alt=""
+      />
+      netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix v n netflix netflix
+      netflix netflixnetflixnetflix netflix netflixnetflixnetflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix netflix netflix netflix netflix netflix netflix netflix
+      netflix netflix v n netflix netflix netflix netflixnetflixnetflix netflix
+      netflixnetflixnetflix netflix netflix netflix netflix netflix netflix
+    </div>
+  );
+};
+
+export default Home;
